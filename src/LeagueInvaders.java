209a42164642506c95b674b2ce18d0afcf3a6dea
Nazo123 <48037675+Nazo123@users.noreply.github.com>
2020-04-13 19:56:49 -0700

    I started LeagueInvaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,20 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	
+	public static void main(String[] args) {
+		LeagueInvaders ship = new LeagueInvaders();
+		ship.setup();
+	}
+	GamePanel game = new GamePanel();
+	void setup() {
+		frame = new JFrame();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(3);
+		frame.setSize(500, 800);
+		frame.add(game);
+	}
 }
